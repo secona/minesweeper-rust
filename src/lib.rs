bda882e5eb175e9892325bac9720774c151e07fe
secona <77039267+secona@users.noreply.github.com>
2023-06-19 18:04:02 +0700

    feat: implement increment_numbers_around_bombs method

--- src/lib.rs
+++ src/lib.rs
@@ -39,28 +39,61 @@ impl Board {
     }
 
     pub fn populate(mut self, bomb_count: usize) -> Result<Board, &'static str> {
-        self.place_bombs(bomb_count)?;
-        self.increment_numbers_around_bombs()?;
+        let bomb_coords = self.place_bombs(bomb_count)?;
+        self.increment_numbers_around_bombs(bomb_coords)?;
 
         Ok(self)
     }
 
-    fn place_bombs(&mut self, bomb_count: usize) -> Result<(), &'static str> {
+    fn place_bombs(&mut self, bomb_count: usize) -> Result<Vec<(usize, usize)>, &'static str> {
         if bomb_count > (self.size * self.size) {
             return Err("bomb_count exceeded cell count.");
         }
+
+        let mut bomb_coords: Vec<(usize, usize)> = vec![];
 
         for _ in 0..bomb_count {
             let mut rng = rand::thread_rng();
             let x = rng.gen_range(0..self.size);
             let y = rng.gen_range(0..self.size);
             self.cells[x][y] = Cell::Bomb;
+            bomb_coords.push((x, y));
         }
 
-        Ok(())
+        Ok(bomb_coords)
     }
 
-    fn increment_numbers_around_bombs(&mut self) -> Result<(), &'static str> {
+    fn increment_numbers_around_bombs(
+        &mut self,
+        bomb_coords: Vec<(usize, usize)>,
+    ) -> Result<(), &'static str> {
+        for coord in bomb_coords {
+            let x: i32 = coord.0.try_into().unwrap();
+            let y: i32 = coord.1.try_into().unwrap();
+
+            for i in -1..=1 {
+                for j in -1..=1 {
+                    let x_to_change: usize = match x + i {
+                        d if d < 0 => continue,
+                        d if d >= self.size.try_into().unwrap() => continue,
+                        _ => (x + i).try_into().unwrap(),
+                    };
+
+                    let y_to_change: usize = match y + j {
+                        d if d < 0 => continue,
+                        d if d >= self.size.try_into().unwrap() => continue,
+                        _ => (y + j).try_into().unwrap(),
+                    };
+
+                    let current_cell = &mut self.cells[x_to_change][y_to_change];
+                    *current_cell = match current_cell {
+                        Cell::Number(n) => Cell::Number(*n + 1),
+                        Cell::Bomb => continue,
+                    };
+                }
+            }
+        }
+
         Ok(())
     }
 }
